21523b71e853a4c9e85eb745887d1265e10369f8
Naveen Narayanan <nnarayanan12@gmail.com>
2015-03-25 21:14:32 -0400

    Added comments but can't get backwards propogation working well for square network

--- neural_network/neural.py
+++ neural_network/neural.py
@@ -3,14 +3,27 @@ import learning.backPropogation as learning
 import functions.sigmoid as functions
 import numpy as np
 
+# Square matrix size for pixel values
 patternSize = 9
-patterns = 1
 
-input_r = np.array([[0.3, 0.4]])
-output_r = np.array([[0.88]])
+#Make sure inputs and outputs are numpy matrix
 
-net = network.Network(functions.SigmoidFunction(1), patternSize, (2, 2, 1))
+#Pass inputs here
+input_r = np.array([[-0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5], [-0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, -0.5]])
+#Pass outputs here
+output_r = np.array([[-0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5], [-0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, -0.5]])
 
+# Network structure, can be changed but I haven't tested with more than 3 layers
+shape = (9, 9, 9)
+
+# Initialize network
+net = network.Network(functions.SigmoidFunction(1), patternSize, shape)
+
+# Select learning algorithm
 learning_alg = learning.BackPropogationLearning(net)
 
+# Start training
 learning_alg.train(input_r, output_r)
+
+# Get output
+print(learning_alg.run(input_r))
